8538dd5dc0c6ba3fe397c2419b6b4231a001c848
Sabin Darkstone <sabin.darkstone@gmail.com>
2016-12-29 19:02:19 -0500

    Added analysis of URLs

--- Visco Web Scrape v2/Search/Items/SearchResults.cs
+++ Visco Web Scrape v2/Search/Items/SearchResults.cs
@@ -2,28 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using Abot.Poco;
+using Visco_Web_Scrape_v2.Scripts.Helpers;
 
 namespace Visco_Web_Scrape_v2.Search.Items {
 
 	[Serializable]
 	public class SearchResults {
 
+		[Serializable]
+		public class UrlParts {
+
+			public UrlParts() {
+				WithKeyword = new Dictionary<string, int>();
+				WithoutKeyword = new Dictionary<string, int>();
+			}
+
+			public Dictionary<string, int> WithKeyword { get; set;}
+			public Dictionary<string, int> WithoutKeyword { get; set; }
+		}
+
 		public string RootUrl { get; }
 		public string Name { get; }
 		public List<Result> ResultList { get; set; }
+		public UrlParts UrlDetails { get; set; }
 
 		public SearchResults() {
 			RootUrl = null;
 			ResultList = new List<Result>();
+			UrlDetails = new UrlParts();
 		}
 
 		public SearchResults(string rootUrl) {
 			ResultList = new List<Result>();
+			UrlDetails = new UrlParts();
 			RootUrl = rootUrl;
 		}
 
 		public SearchResults(Website website) {
 			ResultList = new List<Result>();
+			UrlDetails = new UrlParts();
 			RootUrl = website.Url;
 			Name = website.Name;
 		}
@@ -40,6 +57,32 @@ namespace Visco_Web_Scrape_v2.Search.Items {
 
 		public void AddNewResult(Result result) {
 			ResultList.Add(result);
+		}
+
+		public void AddUrlParts(string[] parts, bool keywordsFound) {
+			foreach (var part in parts) {
+				if (part.Contains("www.")) continue;
+				if (string.IsNullOrWhiteSpace(part)) continue;
+				if (part.Contains("title=")) continue;
+				if (part.Contains("feedback?")) continue;
+				if (part.Length == 1) continue;
+				if (part.Contains("=")) continue;
+
+				if (!UrlDetails.WithKeyword.Keys.Contains(part)) {
+					LogHelper.Debug("Adding url part: " + part);
+					UrlDetails.WithKeyword.Add(part, 0);
+				}
+
+				if (!UrlDetails.WithoutKeyword.Keys.Contains(part)) {
+					UrlDetails.WithoutKeyword.Add(part, 0);
+				}
+
+				if (keywordsFound) {
+					UrlDetails.WithKeyword[part]++;
+				} else {
+					UrlDetails.WithoutKeyword[part]++;
+				}
+			}
 		}
 
 		private int CheckForDuplicates(CrawledPage page) {
