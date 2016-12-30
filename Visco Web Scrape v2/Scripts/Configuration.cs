d129812d8f96b54fc2f6907b26c6733b470e8a4e
Sabin Darkstone <sabin.darkstone@gmail.com>
2016-12-29 19:00:06 -0500

    Updated and refactored for new settings

--- Visco Web Scrape v2/Scripts/Configuration.cs
+++ Visco Web Scrape v2/Scripts/Configuration.cs
@@ -7,19 +7,39 @@ namespace Visco_Web_Scrape_v2.Scripts {
 	[Serializable]
 	public class Configuration {
 
+		[Serializable]
+		public class LastCrawlInfo {
+
+			public LastCrawlInfo() {
+				Date = DateTime.MinValue;
+				CompletionStatus = "Unknown";
+			}
+
+			public List<SearchResults> Results { get; set; }
+			public DateTime Date { get; set; }
+			public string CompletionStatus { get; set; }
+		}
+
 		public enum ExportType { Excel, Plain, Xml }
 
 		public List<Website> Websites { get; set; }
 		public List<Keyword> Keywords { get; set; }
-		public List<SearchResults> Results { get; set; }
+		
 		public int PagesToCrawlPerDomain { get; set; }
 		public bool EnableUrlFiltering { get; set; }
+		public bool EnableUrlAnalysis { get; set; }
+		public bool IncludeDate { get; set; }
+		public bool OnlyNewResults { get; set; }
 		public ExportType ExportMethod { get; set; }
+		public LastCrawlInfo LastCrawl { get; set; }
 
 		public Configuration() {
 			Websites = new List<Website>();
 			Keywords = new List<Keyword>();
-			Results = new List<SearchResults>();
+
+			if (LastCrawl == null) {
+				LastCrawl = new LastCrawlInfo { Results = new List<SearchResults>() };
+			}
 		}
 	}
 }
