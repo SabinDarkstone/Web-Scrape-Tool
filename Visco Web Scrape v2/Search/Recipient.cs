40df23596ba89e0d05cf969bd91b1b49b46fec88
Sabin Darkstone <sabin.darkstone@gmail.com>
2016-12-30 19:14:06 -0500

    Added recipients for email

---
+++ Visco Web Scrape v2/Search/Recipient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Visco_Web_Scrape_v2.Search {
+
+	[Serializable]
+	public class Recipient {
+
+		public string Name { get; }
+		public string Address { get; }
+
+		public Recipient(string name, string address) {
+			Name = name;
+			Address = address;
+		}
+
+		public override string ToString() {
+			return Name;
+		}
+	}
+}
