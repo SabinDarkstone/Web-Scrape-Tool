dd41125a7e3b618cae526a5e5891fc52e95b50e0
Sabin Darkstone <sabin.darkstone@gmail.com>
2017-01-08 14:57:05 -0500

    Added place to store Funcs and Predicates

---
+++ Visco Web Scrape v2/Scripts/Helpers/Comparisons.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Visco_Web_Scrape_v2.Search.Items;
+
+namespace Visco_Web_Scrape_v2.Scripts.Helpers {
+
+	public static class Comparisons {
+
+		public static Func<WebsiteResults, bool> SearchStarted =
+			results =>
+				results.WebsiteStatus == WebsiteResults.Status.Completed || results.WebsiteStatus == WebsiteResults.Status.Interrupted;
+	}
+
+}
