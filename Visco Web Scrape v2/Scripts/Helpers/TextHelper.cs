f5e70a3f3c5d428d82993aeb7ec9a878e93c44ef
Sabin Darkstone <sabin.darkstone@gmail.com>
2017-01-08 14:57:34 -0500

    Added class for string operations

---
+++ Visco Web Scrape v2/Scripts/Helpers/TextHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Visco_Web_Scrape_v2.Scripts.Helpers {
+	public static class TextHelper {
+
+		public static string FormatTime(TimeSpan timeToFormat) {
+			var hours = (timeToFormat.Hours < 10) ? "0" + timeToFormat.Hours : timeToFormat.Hours.ToString();
+			var minutes = (timeToFormat.Minutes < 10) ? "0" + timeToFormat.Minutes : timeToFormat.Minutes.ToString();
+			var seconds = (timeToFormat.Seconds < 10) ? "0" + timeToFormat.Seconds : timeToFormat.Seconds.ToString();
+
+			return hours + ":" + minutes + ":" + seconds;
+		}
+	}
+}
