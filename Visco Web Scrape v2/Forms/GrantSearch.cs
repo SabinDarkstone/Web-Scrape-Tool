51992c32a95dda4f724e8c5be2c470fe8cfae922
Sabin Darkstone <sabin.darkstone@gmail.com>
2017-01-08 14:55:03 -0500

    Backend overhaul
    Changed ElapsedTime class to simpler TimeSpan object.  Reworked status
    update. search and cancellation logic, adding/updating results and
    report percent progress back to MainForm.

--- Visco Web Scrape v2/Forms/GrantSearch.cs
+++ Visco Web Scrape v2/Forms/GrantSearch.cs
@@ -13,57 +13,38 @@ namespace Visco_Web_Scrape_v2.Forms {
 
 	public partial class GrantSearch : Form {
 
-		public static class ElapsedTime {
-
-			public static int TotalSeconds { get; set; }
-			public static int TotalMinutes { get; set; }
-			public static int TotalHours { get; set; }
-			public static int TotalTime { get; set; }
-
-			public static int CurrentSeconds { get; set; }
-			public static int CurrentMinutes { get; set; }
-			public static int CurrentHours { get; set; }
-			public static int CurrentTime { get; set; }
-
-			public static void ClearAll() {
-				TotalSeconds = 0;
-				TotalMinutes = 0;
-				TotalHours = 0;
-				TotalTime = 0;
-
-				CurrentSeconds = 0;
-				CurrentMinutes = 0;
-				CurrentHours = 0;
-				CurrentTime = 0;
-			}
-		}
-
+		public TimeSpan CurrentDomaim;
+		public TimeSpan TotalSearch;
 		public Configuration Config;
 		public CombinedResults Results { get; set; }
 		public CancellationTokenSource Cts = new CancellationTokenSource();
-		public Progress LastProgress { get; private set; }
 
 		private BackgroundWorker worker;
 		private readonly Job jobToRun;
-		/* UNDONE search history for stability
-		private CombinedResults.History currentSearch;
-		*/
+		private MainForm parent;
 
-		public GrantSearch(Configuration configuration, CombinedResults results, Job job) {
+		public GrantSearch(Configuration configuration, CombinedResults results, Job job, MainForm parent) {
 			InitializeComponent();
 
 			Config = configuration;
 			Results = results;
 			jobToRun = job;
+			this.parent = parent;
 		}
 
 		private void UpdateFields(Progress progress) {
-			lblCurrentStatus.Text = progress.CurrentStatus.ToString();
-			lblCurrentDomain.Text = progress.Domain ?? lblCurrentDomain.Text;
-			lblCurrentUrl.Text = progress.Url ?? lblCurrentUrl.Text;
-			lblPagesCrawledCount.Text = CrawlHelper.TotalPages.ToString();
-			lblPagesSkippedCount.Text = CrawlHelper.SkippedPages.ToString();
-			lblResultsFound.Text = progress.RelevantPageCount == null ? lblResultsFound.Text : progress.RelevantPageCount.ToString();
+			try {
+				lblCurrentStatus.Text = progress.SearchStatus.ToString();
+				lblCurrentDomain.Text = progress.Website.Name;
+				lblCurrentUrl.Text = progress.CurrentUrl;
+				lblPagesCrawledCount.Text = progress.SearchedPages.ToString();
+				lblPagesSkippedCount.Text = progress.IgnoredPages.ToString();
+				lblResultsFound.Text = progress.ResultsFound.ToString();
+			} catch (Exception e) {
+				LogHelper.Fatal(e.Message);
+				LogHelper.Trace(e.StackTrace);
+			}
+
 		}
 
 		public void Stop(DoWorkEventArgs e) {
@@ -73,42 +54,26 @@ namespace Visco_Web_Scrape_v2.Forms {
 		private void btnSaveResults_Click(object sender, EventArgs e) {
 			LogHelper.Debug("Saving results...");
 
-			foreach (var website in Results.AllResults) {
-				foreach (var result in website.ResultList) {
-					LogHelper.Info("Found result on " + website.RootWebsite.Name + "\n" + result.PageUrl);
-				}
-			}
-
 			DialogResult = DialogResult.OK;
 			Hide();
 		}
 
-		// TODO: Fix completion status issue
+		// TODO
 		private void btnCancelCrawl_Click(object sender, EventArgs e) {
 			if (worker.IsBusy) {
-				var check =
-					MessageBox.Show(Resources.ConfirmCancelCrawl, Resources.ConfirmationRequired, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+				var confirm = MessageBox.Show(Resources.ConfirmCancelCrawl, Resources.ConfirmationRequired, MessageBoxButtons.YesNo,
+					MessageBoxIcon.Question);
 
-				if (check == DialogResult.No)
-					return;
+				if (confirm == DialogResult.No) return;
 
-				worker.ReportProgress(0, new Progress(Progress.Status.Cancelling));
 				worker.CancelAsync();
 				timerTotal.Enabled = false;
-			} else {
-				LastProgress.CurrentStatus = Progress.Status.Cancelled;
-				DialogResult = DialogResult.Cancel;
-				Close();
 			}
 		}
 
 		private void GrantSearch_Shown(object sender, EventArgs e) {
 			// Initialize results
-			Results.StartNewSearch();
-			ElapsedTime.ClearAll();
-			CrawlHelper.SkippedPages = 0;
-			CrawlHelper.CurrentDomain = 0;
-			CrawlHelper.TotalPages = 0;
+			Results.Begin();
 
 			// Initialize the background worker
 			worker = new BackgroundWorker {
@@ -125,6 +90,10 @@ namespace Visco_Web_Scrape_v2.Forms {
 			worker.RunWorkerAsync(jobToRun);
 		}
 
+		/// <summary>
+		/// Iterates through the list of websites in the jobToRun.
+		/// Handles what happens before and after the crawl is completed.
+		/// </summary>
 		private void worker_DoWork(object sender, DoWorkEventArgs e) {
 			// Update UI to match "in-progress" conditions
 			btnCancelCrawl.Invoke(new MethodInvoker(delegate { btnCancelCrawl.Text = "Stop"; }));
@@ -135,140 +104,67 @@ namespace Visco_Web_Scrape_v2.Forms {
 			if (myJob == null) throw new NullReferenceException("No website list.");
 
 			var myWorker = sender as BackgroundWorker;
+			Results.Begin();
 
+			double i = 0;
 			foreach (var website in myJob.WebsitesToCrawl) {
-				// Reset current domain timer
-				ElapsedTime.CurrentHours = 0;
-				ElapsedTime.CurrentMinutes = 0;
-				ElapsedTime.CurrentSeconds = 0;
-				ElapsedTime.CurrentTime = 0;
+				i++;
+				parent.PercentSearchComplete = (i / myJob.WebsitesToCrawl.Count) * 100;
+				MessageBox.Show(i + " " + parent.PercentSearchComplete.ToString());
 
-				// Set website results as not being new
-				var websiteResults = Results.AllResults.FirstOrDefault(i => i.RootWebsite.Url.Equals(website.Url));
-				websiteResults?.StartNewSearch();
-
-				// Initialize and run the crawler
-				CrawlHelper.CurrentDomain++;
-				var grantCrawler = new GrantCrawler(jobToRun, Config, website, myWorker, Cts, this);
+				// Check for cancellation
 				if (Cts.IsCancellationRequested) {
-					lblCurrentStatus.Invoke(new MethodInvoker(delegate { lblCurrentStatus.Text = "Cancelled"; }));
-
-					LastProgress.CurrentStatus = Progress.Status.Cancelled;
-					return;
+					LogHelper.Debug("Cancellation requested, skipping " + website.Name);
+					continue;
 				}
 
-				if (grantCrawler.Successful) {
-					// Mark the website as completed
-					var firstOrDefault = Results.AllResults.FirstOrDefault(i => i.RootWebsite.Url.Equals(website.Url));
-					if (firstOrDefault != null) {
-						firstOrDefault.CompletedSearch = true;
-					}
-				}
+				// Start the crawl
+				var grantCrawler = new GrantCrawler(jobToRun, Config, website, myWorker, Cts, this);
+
+				// After website search is deemed completed
+				var resultsToAdd = grantCrawler.Results;
+				LogHelper.Debug("Current domain time: " + CurrentDomaim.ToString());
+				resultsToAdd.SearchTime = CurrentDomaim;
+				Results.UpdateResults(resultsToAdd);
 			}
+			
+			Results.End();
 		}
 
+		// TODO
 		private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e) {
 			var progress = e.UserState as Progress;
-			progressbar.Value = CrawlHelper.CurrentDomain;
+
 			UpdateFields(progress);
 		}
 
 		private void timerTotal_Tick(object sender, EventArgs e) {
-			ElapsedTime.TotalTime++;
-			ElapsedTime.CurrentTime++;
+			TotalSearch += TimeSpan.FromSeconds(1);
+			CurrentDomaim += TimeSpan.FromSeconds(1);
 
-			ElapsedTime.TotalSeconds++;
-			ElapsedTime.CurrentSeconds++;
-
-			if (ElapsedTime.TotalSeconds == 60) {
-				ElapsedTime.TotalSeconds = 0;
-				ElapsedTime.TotalMinutes++;
-
-				if (ElapsedTime.TotalMinutes == 60) {
-					ElapsedTime.TotalMinutes = 0;
-					ElapsedTime.TotalHours++;
-				}
-			}
-
-			if (ElapsedTime.CurrentSeconds == 60) {
-				ElapsedTime.CurrentSeconds = 0;
-				ElapsedTime.CurrentMinutes++;
-
-				if (ElapsedTime.CurrentMinutes == 60) {
-					ElapsedTime.CurrentMinutes = 0;
-					ElapsedTime.CurrentHours++;
-				}
-			}
-
-			UpdateTimers();
+			lblCurrentDomainTime.Text = TextHelper.FormatTime(CurrentDomaim);
+			lblTotalTime.Text = TextHelper.FormatTime(TotalSearch);
 		}
 
-		private void UpdateTimers() {
-			string currSec, currMin, currHr;
-			string totSec, totMin, totHr;
-
-			if (ElapsedTime.CurrentSeconds < 10) {
-				currSec = "0" + ElapsedTime.CurrentSeconds;
-			} else {
-				currSec = ElapsedTime.CurrentSeconds.ToString();
-			}
-
-			if (ElapsedTime.CurrentMinutes < 10) {
-				currMin = "0" + ElapsedTime.CurrentMinutes;
-			} else {
-				currMin = ElapsedTime.CurrentMinutes.ToString();
-			}
-
-			if (ElapsedTime.CurrentHours < 10) {
-				currHr = "0" + ElapsedTime.CurrentHours;
-			} else {
-				currHr = ElapsedTime.CurrentHours.ToString();
-			}
-
-			if (ElapsedTime.TotalSeconds < 10) {
-				totSec = "0" + ElapsedTime.TotalSeconds;
-			} else {
-				totSec = ElapsedTime.TotalSeconds.ToString();
-			}
-
-			if (ElapsedTime.TotalMinutes < 10) {
-				totMin = "0" + ElapsedTime.TotalMinutes;
-			} else {
-				totMin = ElapsedTime.TotalMinutes.ToString();
-			}
-
-			if (ElapsedTime.TotalHours < 10) {
-				totHr = "0" + ElapsedTime.TotalHours;
-			} else {
-				totHr = ElapsedTime.TotalHours.ToString();
-			}
-
-			lblCurrentDomainTime.Text = currHr + ":" + currMin + ":" + currSec;
-			lblTotalTime.Text = totHr + ":" + totMin + ":" + totSec;
-		}
-
+		// TODO
 		private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
-			btnCancelCrawl.Text = "Cancel";
-			btnCancelCrawl.Enabled = false;
+			Results.End();
+			btnCancelCrawl.Enabled = true;
+			btnCancelCrawl.Text = "Close";
 			btnSaveResults.Enabled = true;
-
-			lblCurrentDomain.Text = "";
-			lblCurrentUrl.Text = "";
-
-			if (!timerTotal.Enabled) {
-				lblCurrentStatus.Text = "Cancelled";
-			}
 		}
 
+		// TODO
 		private void GrantSearch_Load(object sender, EventArgs e) {
-			// Clear out placeholder fields
+			lblCurrentStatus.Text = "";
 			lblCurrentDomain.Text = "";
+			lblCurrentDomainTime.Text = "";
 			lblCurrentUrl.Text = "";
 			lblPagesCrawledCount.Text = "";
+			lblPagesSkippedCount.Text = "";
 			lblResultsFound.Text = "";
-
-			// Set progress bar maximum to one more than the number of websites
-			progressbar.Maximum = jobToRun.WebsitesToCrawl.Count + 1;
+			lblTotalTime.Text = "";
 		}
 	}
+
 }
