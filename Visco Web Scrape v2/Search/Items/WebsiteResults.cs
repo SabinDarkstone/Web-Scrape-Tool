1f8736ecaf75a0ba06a87f16e1af7be18d7b53b6
Sabin Darkstone <sabin.darkstone@gmail.com>
2017-01-09 10:45:37 -0500

    Clean up

--- Visco Web Scrape v2/Search/Items/WebsiteResults.cs
+++ Visco Web Scrape v2/Search/Items/WebsiteResults.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Visco_Web_Scrape_v2.Scripts;
 using Visco_Web_Scrape_v2.Scripts.Helpers;
 
 namespace Visco_Web_Scrape_v2.Search.Items {
@@ -50,14 +51,24 @@ namespace Visco_Web_Scrape_v2.Search.Items {
 			ResultList = new HashSet<Result>();
 		}
 
+		// TODO: Let user choose to filter by context or not, currently it is hard-coded with not allowing similar contexts
 		/// <summary>
 		/// Add a new result. Compares results before merging to prevent duplication.
 		/// </summary>
 		/// <param name="result">Result to add</param>
-		public void AddResult(Result result) {
+		public bool AddResult(Result result) {
+			if (result.Hits.Any(hit => ResultList.Any(myResult => myResult.Hits.Any(myHit => myHit.Keyword.Text.Equals(hit.Keyword.Text) && myHit.Context.Equals(hit.Context))))) {
+				LogHelper.Debug("Matching context found, skipping result");
+				return false;
+			}
+
 			var matchingResult = ResultList.FirstOrDefault(i => i.PageUrl.Equals(result.PageUrl));
 			if (matchingResult == null) {
 				ResultList.Add(result);
+				return true;
+			} else {
+				LogHelper.Debug("Result url already exists");
+				return false;
 			}
 		}
 	}
