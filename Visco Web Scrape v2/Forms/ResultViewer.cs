d538c29e1070569da1a2206ea236a98ec3f20398
Sabin Darkstone <sabin.darkstone@gmail.com>
2017-01-26 15:44:24 -0500

    Fixed filename and saving issues

--- Visco Web Scrape v2/Forms/ResultViewer.cs
+++ Visco Web Scrape v2/Forms/ResultViewer.cs
@@ -1,21 +1,34 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 using Visco_Web_Scrape_v2.Exporters;
 using Visco_Web_Scrape_v2.Scripts;
+using Visco_Web_Scrape_v2.Scripts.Helpers;
 using Visco_Web_Scrape_v2.Search.Items;
 
 namespace Visco_Web_Scrape_v2.Forms {
 
 	public partial class ResultViewer : Form {
 
+		public string[] Filenames;
+		public bool IsForEmail;
+		public bool IsCompleted { get; private set; }
+		public AutoResetEvent Completion;
+
 		private readonly Configuration config;
 		private readonly CombinedResults results;
 
-		public ResultViewer(Configuration configuration, CombinedResults results) {
+		public ResultViewer(Configuration configuration, CombinedResults results, bool email, string[] filenames = null) {
 			InitializeComponent();
 
 			this.config = configuration;
 			this.results = results;
+			this.Filenames = filenames;
+			this.IsForEmail = email;
+
+			if (filenames == null) {
+				this.Filenames = new[] { "Grants", "Other" };
+			}
 		}
 
 		private void ResultViewer_Shown(object sender, EventArgs e) {
@@ -30,19 +43,22 @@ namespace Visco_Web_Scrape_v2.Forms {
 			progressSheet.Value = progress.CurrentRow;
 		}
 
-		private void StartExport() {
+		public void StartExport() {
 			IExportable exporter;
 
 			switch (config.ExportMethod) {
 				case Configuration.ExportType.Excel:
 					exporter = new ExcelExport(config, results, this);
+
+					LogHelper.Debug("Waiting for background process to finish");
+					((ExcelExport) exporter).Completion.WaitOne();
+					IsCompleted = ((ExcelExport) exporter).IsCompleted;
+					LogHelper.Debug("Background process finished");
 					break;
 				default:
 					throw new ArgumentOutOfRangeException();
 			}
-
 		}
 
 	}
-
 }
